845444eea10b4a3f6c25f7aa1de54c3a06c9cb30
chenqim <chenqim@51bsi.com>
2018-08-22 17:40:43 +0800

    修改上传的controller使之与前台对应

--- src/main/java/com/summer/controller/FileUploadController.java
+++ src/main/java/com/summer/controller/FileUploadController.java
@@ -2,8 +2,10 @@ package com.summer.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,16 +21,24 @@ public class FileUploadController {
     private static Logger logger = Logger.getLogger(FileUploadController.class);
 
     @RequestMapping("upload")
-    public String uploadFile (HttpServletRequest request, HttpServletResponse response) {
+    // RequestParam要与前端的name相对应
+    public String uploadFile (@RequestParam("file") MultipartFile upFile, HttpServletRequest request) {
 
         if (request.getContentLength() > 0) {
             InputStream inputStream = null;
             FileOutputStream outputStream = null;
             try {
-                inputStream = request.getInputStream();
-                // 给新文件拼上时间毫秒，防止重名
+                // 前端上传的文件的流可以从 MultipartHttpServletRequest 中获取
+                // MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+                // inputStream = multipartHttpServletRequest.getFile("file").getInputStream();
+                // 也可以从 MultipartFile 中获取
+                inputStream = upFile.getInputStream();
+                // 处理文件名称，防止重名，最暴力的方法，直接拼接当前的毫秒数
+                String originalName = upFile.getOriginalFilename();
+                String name = originalName.substring(0, originalName.lastIndexOf("."));
+                String type = originalName.substring(originalName.lastIndexOf("."));
                 long now = System.currentTimeMillis();
-                File file = new File("D:/", "file-" + now + ".txt");
+                File file = new File("D:/", name + "-" + now + type);
                 file.createNewFile();
 
                 outputStream = new FileOutputStream(file);
